d64bcabacb49fe7f28cb28335c8be68139d47dd7
DannyCoop <Cooperdaniel5141@gmail.com>
2020-11-28 03:57:47 -0500

    validation is almost done

--- tribe-frontend-app/src/SignUpForm/index.tsx
+++ tribe-frontend-app/src/SignUpForm/index.tsx
@@ -10,6 +10,7 @@ const LOCALHOST_SIGNUP_URL = 'http://127.0.0.1:8000/sign-up';
 function SignUpForm() {
     const INITIAL_FORM_VALUES = { username: "", password: "", email: "", repeatPassword: "" }
     const [formData, setFormData] = useState(INITIAL_FORM_VALUES);
+    const [validated, setValidated] = useState(false);
 
     const handleChange = (evt: React.FormEvent<HTMLInputElement>) => {
         const { name, value } = evt.currentTarget;
@@ -17,6 +18,9 @@ function SignUpForm() {
 
     }
 
+    const validationCheck = (form: any) => {
+
+    }
 
     const handleSubmit = (evt: React.FormEvent) => {
         evt.preventDefault();
@@ -33,25 +37,36 @@ function SignUpForm() {
         } else {
             alert("Passwords do not match!")
         }
+
+        const form = evt.currentTarget as any;
+        if (form.checkValidity() === false) {
+            evt.preventDefault();
+            evt.stopPropagation();
+        }
+        console.log("it worked")
+        setValidated(true);
     }
 
     return (
-        <Form className="SignUpForm" onSubmit={handleSubmit}>
-            <Form.Group controlId="formGroupUsername">
+        <Form noValidate validated={validated} className="SignUpForm" onSubmit={handleSubmit}>
+            <Form.Group  controlId="formGroupUsername" >
                 <Form.Label>Username</Form.Label>
-                <Form.Control size="lg" autoComplete="username" placeholder="Username" name="username" value={formData.username} onChange={(evt) => handleChange(evt as any)} />
+                <Form.Control required size="lg" pattern=".{5,10}" autoComplete="username" placeholder="Username" type="username" name="username" value={formData.username} onChange={(evt) => handleChange(evt as any)} />
             </Form.Group>
             <Form.Group controlId="formGroupEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control size="lg" placeholder="Enter Email" name="email" type="email" value={formData.email} onChange={(evt) => handleChange(evt as any)} />
-                <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+                <Form.Control required size="lg" placeholder="Enter Email" name="email" type="email" value={formData.email} onChange={(evt) => handleChange(evt as any)} />
             </Form.Group>
             <Form.Group controlId="formGroupPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control size="lg" autoComplete="current-password" placeholder="Password" name="password" value={formData.password} type="password" onChange={(evt) => handleChange(evt as any)} />
+                <Form.Control required size="lg" pattern=".{8,20}"autoComplete="current-password" placeholder="Password" name="password" value={formData.password} type="password" onChange={(evt) => handleChange(evt as any)} />
             </Form.Group>
+            <Form.Text id="passwordHelpBlock" muted>
+                Your password must be 8-20 characters long, contain letters and numbers, and
+                must not contain spaces, special characters, or emoji.
+            </Form.Text>
             <Form.Group controlId="formGroupRePassword">
-                <Form.Control size="lg" autoComplete="current-password" placeholder="Re-type password" name="repeatPassword" value={formData.repeatPassword} type="password" onChange={(evt) => handleChange(evt as any)} />
+                <Form.Control required size="lg" pattern=".{password}"autoComplete="current-password" placeholder="Re-type password" name="repeatPassword" value={formData.repeatPassword} type="password" onChange={(evt) => handleChange(evt as any)} />
             </Form.Group>
             <Button type="submit">Register</Button>
         </Form>
