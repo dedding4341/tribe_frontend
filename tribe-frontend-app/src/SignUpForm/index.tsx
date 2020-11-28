de8136dae821eb10ba8b5f35ceed345b7cedb543
DannyCoop <Cooperdaniel5141@gmail.com>
2020-11-27 22:08:50 -0500

    add validantion to the forms

--- tribe-frontend-app/src/SignUpForm/index.tsx
+++ tribe-frontend-app/src/SignUpForm/index.tsx
@@ -6,94 +6,59 @@ import './SignUpForm.css';
 
 const EC2_SIGNUP_URL = 'http://ec2-52-53-238-185.us-west-1.compute.amazonaws.com:5000/sign-up';
 const LOCALHOST_SIGNUP_URL = 'http://127.0.0.1:8000/sign-up';
-const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
 
 
 function SignUpForm() {
     const INITIAL_FORM_VALUES = { username: "", password: "", email: "", repeatPassword: ""}
-    const [errors, setErrors] = useState({username: "", password: "", email: "", repeatPassword: ""})
     const [formData, setFormData] = useState(INITIAL_FORM_VALUES);
 
     const handleChange = (evt: React.FormEvent<HTMLInputElement>) => {
         const { name, value } = evt.currentTarget;
         setFormData(currData => ({ ...currData, [name]: value }));
-        let signUpErrors = errors;
-
-        switch (name) {
-            case 'username':
-                signUpErrors.username = value.length < 5 ? 'Full Name must be 5 characters long!' : '';
-                break;
-            case 'password':
-                signUpErrors.password = value.length < 8 ? 'Password must be 8 characters long!' : '';
-                break;
-            case 'email': 
-                signUpErrors.email = validEmailRegex.test(value) ? '' : 'Email is not valid!';
-                break;
-            case 'repeatPassword':
-                signUpErrors.repeatPassword = value === formData.password ? '' : 'Does not match password';
-                break;
-            default:
-                break;
-        }
-
-        setErrors(signUpErrors => ({ ...signUpErrors, [name]: value}));
-        console.log(errors)
-        
-
     }
-
-    const validateForm = (errors: any) => {
-        let valid = true;
-        Object.values(errors).forEach(
-          // if we have an error string set valid to false
-            (val: any) => val.length > 0 && (valid = false)
-        );
-        return valid;
-    }
-
 
     const handleSubmit = (evt: React.FormEvent) => {
         evt.preventDefault();
-        // if (formData.password === formData.repeatPassword) {
-        //     fetch(LOCALHOST_SIGNUP_URL, {
-        //         method: 'POST',
-        //         body: JSON.stringify(formData),
-        //         headers: {
-        //             "Content-type": "application/json;"
-        //         }
-        //     })
-        //         .then(res => res.json())
-        //         .then(json => console.log(json))
-        // } else {
-        //     alert("Passwords do not match!")
-        // }
-
-        if(validateForm(errors)) {
-            console.log('valid Form')
+        if (formData.password === formData.repeatPassword && formData.username.length > 5 && formData.password.length > 8 ) {
+            fetch(LOCALHOST_SIGNUP_URL, {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                    "Content-type": "application/json;"
+                }
+            })
+                .then(res => res.json())
+                .then(json => console.log(json))
         } else {
-            console.log('Invalid Form')
+            alert("Invaild Form")
         }
+        console.log(formData)
     }
 
     return (
         <Form className="SignUpForm" onSubmit={handleSubmit}>
-            <Form.Group controlId="formGroupUsername">
+            <Form.Group controlId="formBasicUsername">
                 <Form.Label>Username</Form.Label>
-                <Form.Control size="lg" autoComplete="username" placeholder="Username" name="username" value={formData.username} onChange={(evt) => handleChange(evt as any)} />
+                <Form.Control type="username" size="lg" name="username"placeholder="Enter Username" />
             </Form.Group>
-            <Form.Group controlId="formGroupEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control size="lg" placeholder="Enter Email" name="email" type="email" value={formData.email} onChange={(evt) => handleChange(evt as any)} />
-                <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label className="SignUpFormlabel">Email address</Form.Label>
+                <Form.Control type="email" size="lg" name="email"placeholder="Enter Email" />
             </Form.Group>
-            <Form.Group controlId="formGroupPassword">
+
+            <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control size="lg" autoComplete="current-password" placeholder="Password" name="password" value={formData.password} type="password" onChange={(evt) => handleChange(evt as any)} />
+                <Form.Control type="password" size="lg" name="password"placeholder="Enter Password" />
             </Form.Group>
-            <Form.Group controlId="formGroupRePassword">
-                <Form.Control size="lg" autoComplete="current-password" placeholder="Re-type password" name="repeatPassword" value={formData.repeatPassword} type="password" onChange={(evt) => handleChange(evt as any)} />
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control type="password" size="lg" name="repeatPassword"placeholder="Confirm Password" />
             </Form.Group>
-            <Button type="submit">Register</Button>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
         </Form>
     )
 }
