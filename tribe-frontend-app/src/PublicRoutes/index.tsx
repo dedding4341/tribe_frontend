c6ef5fe7ad7bd086ceae028fe8e215a6d3e47fb4
diana <diliang4783573@gmail.com>
2021-01-03 18:54:12 -0800

    Add protection to routes, <Alert/> to public routes

--- tribe-frontend-app/src/PublicRoutes/index.tsx
+++ tribe-frontend-app/src/PublicRoutes/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Alert } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, useHistory } from 'react-router-dom';
 import Landing from '../Landing';
 import Login from '../Login';
 import NavBar from '../NavBar';
@@ -13,11 +13,28 @@ import "./PublicRoutes.css";
  * Routing logic for components
  */
 function PublicRoutes() {
+  const [show, setShow ] = useState(false);
   const isLoggedIn = useSelector((st: any) => st.isLoggedIn);
+  const loading = useSelector((st: any) => st.loading);
+  const history = useHistory();
+
+  useEffect(function () {
+    console.log("mounting the public routes...")
+    if (isLoggedIn) {
+      setShow(true);
+    }
+  }, [loading]);
+
+  function handleGoToOverview() {
+    history.push('/tribe/overview')
+  }
+
   return (
     <>
       <NavBar />
-      {isLoggedIn && <Alert variant="primary" className="PublicRoutes-notice">It seems like you're already signed in! Go to <span className="PublicRoute-notice-link">Tribe</span>.
+      {show && 
+      <Alert variant="primary" className="PublicRoutes-notice" onClose={() => setShow(false)} dismissible>
+        It seems like you're already signed in! Go to <span onClick={handleGoToOverview} className="PublicRoute-notice-link">Tribe</span>.
       </Alert>
       }
       <Switch>
