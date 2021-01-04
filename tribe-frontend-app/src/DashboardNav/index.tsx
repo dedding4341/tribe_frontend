a30021d377f4cc2c72f1704d0d8812ea8ddc95d5
DannyCoop <cooperdaniel5141@gmail.com>
2021-01-03 22:06:41 -0500

    added the family code genrator

--- tribe-frontend-app/src/DashboardNav/index.tsx
+++ tribe-frontend-app/src/DashboardNav/index.tsx
@@ -1,18 +1,51 @@
 import React from 'react';
-import { Nav, Button } from 'react-bootstrap';
+import { Nav, Button, NavItem } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { NavLink, useHistory } from 'react-router-dom';
 import { logoutUser } from '../actionCreators';
+import { getCookie } from '../helpers';
+import { BASE_URL } from '../config';
 import './DashboardNav.css';
 
 function DashboardNav() {
   const dispatch = useDispatch();
-  const history = useHistory();
+	const history = useHistory();
+	const token = getCookie("x-access-token");
+
+	const [codeDisplay, setCodeDisplay] = React.useState("Generate family code!")
 
   const handleLogout = () => {
     dispatch(logoutUser());
     history.push("/users/auth");
-  }
+	}
+	
+	const getFamilyCode = () => {
+		let retcode: number;
+		fetch(`${BASE_URL}/generate-family-code`,{
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+        "x-access-token": `${token}`
+			}
+		})
+		.then(res => {
+			if(res.status == 500){
+				retcode = 500
+			} else {
+				retcode = 201
+			}
+			return res.json();
+		})
+		.then(json => {
+			if(retcode === 500){
+				if(json.msg === "Error occurred while creating family code"){
+					setCodeDisplay("Try Again.")
+				}
+			} else if (retcode === 201){
+				setCodeDisplay(json.family_code)
+			}
+		})
+	}
 
   return (
     <div className="DashboardNav">
@@ -21,6 +54,7 @@ function DashboardNav() {
         <NavLink className="mt-2 mb-2" to={`/tribe/calender`}>Calender</NavLink>
         <NavLink className="mt-2 mb-2" to={`/tribe/todo`}>To-do</NavLink>
         <NavLink className="mt-2 mb-2" to={`/tribe/store`}>Store</NavLink>
+				<Nav.Item className="Family_code" onClick={getFamilyCode}>{codeDisplay}</Nav.Item>
         <li className="DashboardNav-vertical-divider"></li>
         <Button className="mt-2 mb-2" onClick={handleLogout}>Logout</Button>
       </Nav>
