41498bc56c28f94a23fd8f4d2c83aaabec943e7b
DannyCoop <cooperdaniel5141@gmail.com>
2021-01-07 21:45:01 -0500

    Removed all of the console logs. Fixed the math for the time not that we are using seconds. removed the calendar and store for now

--- tribe-frontend-app/src/config.tsx
+++ tribe-frontend-app/src/config.tsx
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://api.tribeapp.family";
-// export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "https://api.tribeapp.family";
+export const BASE_URL = "http://localhost:5000";
 export const DEFAULT_PFP = "https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg";
